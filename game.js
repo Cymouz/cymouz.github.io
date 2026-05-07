f2f44c7071924505c5b3ed6b67f7e71d31711d6e
Cymouz <87407261+Cymouz@users.noreply.github.com>
2026-05-07 15:21:46 -0800

    Update game.js

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ const ClickerGame = (() => {
   let isCheatActive = false;
   let cheatInterval = null;
   let preCheatUpgrades = {};
-  const konamiCode = ['Enter', 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+  const konamiCode = ['Space', 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
   let konamiIndex = 0;
 
   // Listen for cheat code
