d19cb0f42c11edc8b24057907a60fdb5e3825fc6
Cymouz <awesomess22@hotmail.com>
2026-05-19 08:27:47 -0800

    idk

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ const ClickerGame = (() => {
   let isCheatActive = false;
   let cheatInterval = null;
   let preCheatUpgrades = {};
-  const konamiCode = [' ', 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+  const konamiCode = [' ', 'Enter', 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
   let konamiIndex = 0;
 
   document.addEventListener('keydown', (e) => {
